11ab3939e9786dc67d73933e38e91425b4c72fb8
nkand84 <nityabhagi@gmail.com>
2018-04-18 00:02:45 -0700

    got post and get to work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,12 @@
 
 var path = require("path");
+var bodyParser = require("body-parser");
+module.exports = function (app, friends) {
+    // Sets up the Express app to handle data parsing
+    // =============================================================
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
 
-module.exports = function (app,friends) {
     // Displays all friends
     app.get("/api/friends", function (req, res) {
         return res.json(friends);
@@ -13,7 +18,8 @@ module.exports = function (app,friends) {
     // =============================================================
     app.post("/api/friends", function (req, res) {
         var newFriend = req.body;
-        newFriend.name = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+        // newFriend.name = newFriend.name.replace(/\s+/g, "").toLowerCase();
         console.log(newFriend);
         friends.push(newFriend);
         res.json(newFriend);
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-var bodyParser = require("body-parser");
 var path = require("path");
 var app = express();
 var htmlRoutes = require("./app/routing/htmlRoutes.js")(app);
@@ -14,10 +13,6 @@ var apiRoutes = require("./app/routing/apiRoutes.js")(app,friends);
 // var app = express();
 var PORT = process.env.PORT || 3000;
 // =============================================================
-// Sets up the Express app to handle data parsing
-// =============================================================
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 
 
