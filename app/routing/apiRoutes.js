9f0e79f5eda7bcad9fc25eec62a86413f6e658d0
nkand84 <nityabhagi@gmail.com>
2018-04-19 00:06:20 -0700

    something

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -25,7 +25,11 @@ module.exports = function (app, friends) {
         // res.json(newFriend);
         console.log(parseInt(newFriend.scores));
         
-        
+        // dict to save the closet match info
+        var closetMatch = [];
+        var previousVal         = -1;
+        var closedFriendMatch   = "";
+        var closedFriendURL   = "";
         
         for (var i = 0; i < friends.length; i++) {
             // console.log(friends[i].scores);
@@ -34,6 +38,7 @@ module.exports = function (app, friends) {
             var existingUserArray = friends[i].scores;
             var newUserArray = newFriend.scores;
 
+            console.log(friends[i].name);
             console.log(existingUserArray);
             console.log(newUserArray);
 
@@ -41,15 +46,51 @@ module.exports = function (app, friends) {
                 diff += Math.abs(existingUserArray[j] - newUserArray[j]);
                 console.log(" here: "+ j + " is "+ diff);
             }
-            console.log("totaldiff per friend:  " + diff)
+            /* closetMatch.push({
+                name: friends[i].name,
+                difference: diff,
+            }) */
+            if (previousVal === -1) {
+                previousVal         = diff;
+                closedFriendMatch   = friends[i].name;
+                closedFriendURL     = friends[i].photo;
+            }
+            else if (previousVal > diff) {
+                // if previous value exists then compare prev value with the current value and 
+                // if the current value is smaller than prev value then assign current value to prev value
+                previousVal         = diff;
+                closedFriendMatch   = friends[i].name;
+                closedFriendURL     = friends[i].photo;
+            }
+
+            console.log("totaldiff per friend:  " + diff);
             
         }
         
+        //console.log(closetMatch);
+        /* for (var i=0;i<closetMatch.length;i++) {
+            //if there is no previous value then pushing current value to previousval
+            if (previousVal === -1) {
+                previousVal         = closetMatch[i].difference;
+                closedFriendMatch   = closetMatch[i].name;
+            }
+            else if (previousVal > closetMatch[i].difference) {
+                // if previous value exists then compare prev value with the current value and 
+                // if the current value is smaller than prev value then assign current value to prev value
+                previousVal         = closetMatch[i].difference;
+                closedFriendMatch   = closetMatch[i].name;
+            }
+            console.log(previousVal);
+        } */
+
+        console.log(closedFriendMatch,closedFriendURL);
         friends.push(newFriend);
+        matchfriend = {
+            friendMatch: closedFriendMatch,
+            friendURL: closedFriendURL
+        }
+        res.json(matchfriend);
 
-        // pushed score array of the new user into resultArray
-
-
-        // handle the compatibility logic
+        
     });
 }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,14 +2,18 @@
 // Basic route that sends the user first to the AJAX Page
 // =============================================================
 var path = require("path");
-module.exports = function(app){
+module.exports = function (app) {
     app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
-    
+
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
-    
-        //other routes..
+    // If no matching route is found default to home
+    // app.get("*", function (req, res) {
+    //     res.sendFile(path.join(__dirname, "../public/home.html"));
+    // });
+
+
 }
